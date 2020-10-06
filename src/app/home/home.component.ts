6c23ca35a69a5745e40650df3843a02d38d2a01c
pkpriyansh <priyansh.pk.1997@gmail.com>
2020-10-06 14:30:14 +0530

    security on test added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
+import { Test } from './../model/Test';
+import { UserService } from './../user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Routes } from '@angular/router';
+import { homedir } from 'os';
+import { DatePipe, formatDate } from '@angular/common'
 
 @Component({
   selector: 'app-home',
@@ -9,14 +13,37 @@ import { ActivatedRoute, Router, Routes } from '@angular/router';
 export class HomeComponent implements OnInit {
 
   testId:number;
-  constructor(private  router:Router, private route:ActivatedRoute) { }
+  test:Test
+  constructor(private  router:Router, private route:ActivatedRoute, private home:UserService) { }
 
   ngOnInit(): void {
     let id= this.route.snapshot.paramMap.get('id');
 
     this.testId = parseInt(id);
-    
     sessionStorage.setItem("testid",id);
+    this.home.getTestById().subscribe(
+      data =>{
+        this.test = data;
+        //var todayDate = new Date();
+        //var newTodayDate =this.datepipe.transform(todayDate, 'yyyy-MM-dd');
+        if(this.test !=null){
+          let todayDate = formatDate(new Date(), 'yyyy-MM-dd', 'en_US');
+          let testDate = formatDate(this.test.lastDate, 'yyyy-MM-dd', 'en_US');
+          alert("test if condition")
+          if(todayDate != testDate){
+            alert("date if condition")
+            sessionStorage.clear();
+            this.router.navigate(['home']);
+          }else{
+            alert("welcome");
+          }
+        }else{
+          sessionStorage.clear();
+            this.router.navigate(['home']);
+        }
+      }
+    )
+    
     
 
   }
